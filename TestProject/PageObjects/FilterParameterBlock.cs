07ecbe8e4ceaf33fcca14eed68704102d9a38d95
albooo <albertmurzin@gmail.com>
2019-02-28 00:02:35 +0500

    Add Expand methods to FilterParameterBlock

--- TestProject/PageObjects/FilterParameterBlock.cs
+++ TestProject/PageObjects/FilterParameterBlock.cs
@@ -1,10 +1,14 @@
 ﻿using CommonProject.Wrappers;
 using OpenQA.Selenium;
+using System.Linq;
 
 namespace TestProject.PageObjects
 {
     public abstract class FilterParameterBlock
     {
+        private static readonly By _showFullOptionsListLinkLocator = 
+            By.XPath(".//span[contains(@class, 'ModelFilter__OpenHideAttrTxt') and text() = 'Еще' and not(contains(@class, 'hidden'))]");
+        
         protected readonly Element _block;
 
         private readonly Element _header;
@@ -14,7 +18,9 @@ namespace TestProject.PageObjects
             _block = new Element(By.XPath(
                 $"//span[@class = 'ModelFilter__ParamName' and text() = '{parameterName}']/ancestor::div[contains(@class, 'ModelFilter__TipAttrWapper')]"));
             _header = new Element(By.XPath(
-                $"//span[@class = 'ModelFilter__ParamName' and text() = '{parameterName}']/ancestor::div[@class = 'ModelFilter__HelpInfoBlock']"));            
+                $"//span[@class = 'ModelFilter__ParamName' and text() = '{parameterName}']"));
+            ExpandBlock();
+            ShowFullOptionsList();
         }
 
         public void ExpandBlock()
@@ -25,9 +31,18 @@ namespace TestProject.PageObjects
             }
         }
 
+        public void ShowFullOptionsList()
+        {
+            var linksList = _block.FindAllChildElements(_showFullOptionsListLinkLocator);
+            if (linksList.Any())
+            {
+                linksList.FirstOrDefault().Click();
+            }
+        }
+
         private bool IsBlockExpanded()
         {
-            return _block.GetAttribute("class").Contains("open");
+            return _block.GetAttribute("class").Contains("Open");
         }
     }
 }
